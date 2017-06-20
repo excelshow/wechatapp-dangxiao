90cbfde332d6092a0502a140392d6681688ff63c
weimingxuan <793270445@qq.com>
2017-06-20 15:15:10 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # wechatapp-dangxiao
 微信小程序-河北柏乡党校
+
+# wechatapp/小程序源码
+
+
+# server/后端接口
+## ThinkPHP 5.0.7
---
+++ wechatapp/pages/posts/post.js
@@ -0,0 +1,72 @@
+var bannerData = require("../../data/banner-data.js")
+var util = require("../../utils/util.js");
+var app = getApp();
+// pages/posts/post.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    bannerImgUrls: bannerData.bannerImgUrls
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var postsUrl = app.globalData.serverBase + "/api/posts.json";
+    util.http(postsUrl, function (data){
+      console.log(data);
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
