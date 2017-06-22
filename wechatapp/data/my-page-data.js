f9d615954be63e9577cc7b57bc6e6ec9c25e14f6
weimingxuan <793270445@qq.com>
2017-06-22 12:02:14 +0800

    no message

---
+++ wechatapp/data/my-page-data.js
@@ -0,0 +1,33 @@
+var listgrids = [
+  {
+    "cells__title": "",
+    "navigators": [{
+      "icon": "/images/icon/businessman.png",
+      "cell__bd": "个人信息",
+      "cell__ft": "修改资料"
+    }]
+  },
+  {
+    "cells__title": "",
+    "navigators": [{
+      "icon": "/images/icon/businessman.png",
+      "cell__bd": "11个人信息",
+      "cell__ft": "修改资料"
+    }, {
+      "icon": "/images/icon/businessman.png",
+      "cell__bd": "11个人信息",
+      "cell__ft": "修改资料"
+    }]
+  },
+  {
+    "cells__title": "",
+    "navigators": [{
+      "icon": "/images/icon/businessman.png",
+      "cell__bd": "11个人信息",
+      "cell__ft": "修改资料"
+    }]
+  }
+];
+module.exports = {
+  listgrids: listgrids
+}
