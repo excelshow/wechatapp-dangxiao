a206c075c15682902a8d6327bf12807f833aa012
weimingxuan <793270445@qq.com>
2017-06-22 19:42:13 +0800

    no message

--- wechatapp/data/my-page-data.js
+++ wechatapp/data/my-page-data.js
@@ -4,27 +4,31 @@ var listgrids = [
     "navigators": [{
       "icon": "/images/icon/businessman.png",
       "cell__bd": "个人信息",
-      "cell__ft": "修改资料"
+      "cell__ft": "修改资料",
+      "page":""
     }]
   },
   {
     "cells__title": "",
     "navigators": [{
-      "icon": "/images/icon/businessman.png",
-      "cell__bd": "11个人信息",
-      "cell__ft": "修改资料"
+      "icon": "/images/icon/bookmark.png",
+      "cell__bd": "我的收藏",
+      "cell__ft": "",
+      "page": ""
     }, {
-      "icon": "/images/icon/businessman.png",
-      "cell__bd": "11个人信息",
-      "cell__ft": "修改资料"
+      "icon": "/images/icon/clock.png",
+      "cell__bd": "浏览历史",
+      "cell__ft": "",
+      "page": ""
     }]
   },
   {
     "cells__title": "",
     "navigators": [{
-      "icon": "/images/icon/businessman.png",
-      "cell__bd": "11个人信息",
-      "cell__ft": "修改资料"
+      "icon": "/images/icon/about.png",
+      "cell__bd": "关于柏乡县党校小程序",
+      "cell__ft": "",
+      "page": "/pages/about/about"
     }]
   }
 ];
